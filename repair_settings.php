elkarte/tools

commit 2c1bab658e12f48fe824084450f4c9d02f17d4ad

[repair_settings] Fix the problem with double quotes in the mysql password reported by Arantor [Topic 480181]
Signed-off-by: emanuele <emanuele45@gmail.com>

repair_settings.php
@@ -555,7 +555,7 @@ function show_settings()
 			elseif ($info[1] == 'string')
 			{
 				echo '
-								<input type="text" name="', $info[0], 'settings[', $setting, ']" id="', $setting, '" value="', isset($settings[$setting]) ? $settings[$setting] : '', '" size="', $settings_section == 'path_url_settings' || $settings_section == 'theme_path_url_settings' ? '60" style="width: 80%;' : '30', '" class="input_text" />';
+								<input type="text" name="', $info[0], 'settings[', $setting, ']" id="', $setting, '" value="', isset($settings[$setting]) ? htmlspecialchars($settings[$setting]) : '', '" size="', $settings_section == 'path_url_settings' || $settings_section == 'theme_path_url_settings' ? '60" style="width: 80%;' : '30', '" class="input_text" />';
 
 				if (isset($info[2]))
 					echo '

commit f6c3826006c5dca0a3de2902e427cedd3cf90815

the backslash was missing in addcslashes(), result was a completely broken settings.php -> Bug was introduced with commit 2c1bab6. Signed-off-by:Thorsten Eurich <thorsten@eurich.de>

repair_settings.php
@@ -61,9 +61,9 @@ function initialize_inputs()
 	{
 		if (is_array($v))
 			foreach ($v as $k2 => $v2)
-				$_POST[$k][$k2] = addcslashes($v2, '\'');
+				$_POST[$k][$k2] = addcslashes($v2, '\\\'');
 		else
-			$_POST[$k] = addcslashes($v, '\'');
+			$_POST[$k] = addcslashes($v, '\\\'');
 	}
 
 	$db_connection = false;