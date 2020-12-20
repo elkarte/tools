edb53f3ff320e9332881db1a9b32deb8c6323be3
Spuds <spuds@spudsdesign.com>
2020-12-20 13:21:01 -0600

    ! commit fix package changes

--- needs_updating/fix_packages.php
+++ needs_updating/fix_packages.php
@@ -3,48 +3,55 @@
 /**
  * Fix Packages (fixp)
  *
+ * @name      ElkArte Forum
+ * @copyright ElkArte Forum contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
  * @package fixp
  * @author emanuele
- * @copyright 2011 emanuele, Simple Machines
- * @license http://www.simplemachines.org/about/smf/license.php BSD
+ * @copyright 2011 emanuele
  *
  * @version 0.1
  */
 
 // A nice menu button
-define('SMF_INTEGRATION_SETTINGS', serialize(array(
-	'integrate_menu_buttons' => 'fixp_menu_button',)));
+define('ELK_INTEGRATION_SETTINGS', serialize(array('integrate_menu_buttons' => 'fixp_menu_button',)));
+
 // Let's use the default theme
 $ssi_theme = 1;
 $forum_version = 'Fix Packages 0.1';
+
 // If SSI.php is in the same place as this file, this is being run standalone.
 if (file_exists(dirname(__FILE__) . '/SSI.php'))
+{
 	require_once(dirname(__FILE__) . '/SSI.php');
+}
 elseif (file_exists('./SSI.php'))
+{
 	require_once('./SSI.php');
+}
 // Hmm... no SSI.php?
 else
-	die('<b>Error:</b> Cannot find SSI - please verify you put this in the same place as SMF\'s index.php.');
+{
+	die('<b>Error:</b> Cannot find SSI - please verify you put this in the same place as ElkArtes\'s index.php.');
+}
 
 /**
  * Change false to true if you want to avoid the admin check.
  */
 $context['override_security'] = false;
 
-
 /**
- * 
  * Do you want to add a new language?
- * Copy the following function,
- * change 'english' to the language you want
- * and tranlsate it. ;D
- * 
+ * Copy the following function, change 'english' to the language you want
+ * and translate it. ;D
+ *
  */
-function fixp_english ()
+function fixp_english()
 {
 	global $txt;
 
-	$txt['fixp'] = 'Fix Pack';
+	$txt['fixp'] = 'Fix Package';
 	$txt['log_packages_title_installed'] = 'Installed packages';
 	$txt['log_packages_title_removed'] = 'Uninstalled packages';
 	$txt['pack_button_remove'] = 'Mark uninstalled selected';
@@ -56,53 +63,78 @@ function fixp_english ()
 	$txt['mod_removed'] = 'Uninstall date';
 }
 
-
-// Do not change anything below this line
-// ------------------------------------------------------------------------------------------------
+/*
+ * Do not change anything below this line
+ * ------------------------------------------------------------------------------------------------
+ */
 
 // Let's start the main job
 fixp_main();
+
 // and then let's throw out the template! :P
 obExit(null, null, true);
 
-function fixp_menu_button (&$buttons)
+/**
+ * Load the menu
+ *
+ * @param $buttons
+ */
+function fixp_menu_button(&$buttons)
 {
 	global $boardurl, $txt, $context;
+
 	fixp_loadLanguage();
 	$context['current_action'] = 'fixp';
 
 	$buttons['fixp'] = array(
 		'title' => $txt['fixp'],
 		'show' => allowedTo('admin_forum'),
-		'href' => $boardurl . '/fix_packages.php',
+		'href' => $boardurl . '/fixp.php',
 		'active_button' => true,
-		'sub_buttons' => array(
-		),
+		'sub_buttons' => array(),
 	);
 }
 
-function fixp_loadLanguage ()
+/**
+ * Load the language file, ok the language function :P
+ *
+ * @return mixed
+ */
+function fixp_loadLanguage()
 {
 	global $user_info;
 
 	fixp_english();
+
 	$flang = 'fixp_' . (!empty($user_info['language']) ? $user_info['language'] : '');
 	if (function_exists($flang) && $flang != 'fixp_english')
+	{
 		return $flang();
+	}
 }
 
-function fixp_main ()
+/**
+ * Main processing function, shows the form and acts on requests
+ *
+ * @throws \Elk_Exception
+ */
+function fixp_main()
 {
-	global $txt, $sourcedir, $boarddir, $boardurl, $context, $user_info, $smcFunc;
+	global $txt, $boardurl, $context, $user_info;
 
+	$db = database();
+
+	// Use the standard ElkArte files
 	loadLanguage('Admin');
 	loadLanguage('Packages');
 	loadTemplate('Admin');
 	fixp_loadLanguage();
 
 	// Sorry, only logged in admins...unless you want so.
-	if(empty($context['override_security']))
+	if (empty($context['override_security']))
+	{
 		isAllowedTo('admin_forum');
+	}
 
 	$context['install'] = isset($_GET['uninstall']) ? 0 : 1;
 
@@ -111,9 +143,11 @@ function fixp_main ()
 		checkSession();
 
 		foreach ($_POST['remove'] as $id)
+		{
 			if (isset($id) && is_numeric($id))
 			{
 				if (!empty($context['install']))
+				{
 					$db->query('', '
 						UPDATE {db_prefix}log_packages
 						SET
@@ -127,8 +161,10 @@ function fixp_main ()
 							'member_name' => $user_info['name'],
 							'time_removed' => time(),
 							'inst_package_id' => $id,
-					));
+						));
+				}
 				else
+				{
 					$db->query('', '
 						UPDATE {db_prefix}log_packages
 						SET
@@ -139,19 +175,22 @@ function fixp_main ()
 						WHERE id_install = {int:inst_package_id}',
 						array(
 							'inst_package_id' => $id,
-					));
+						));
+				}
 			}
+		}
 
-		require_once($sourcedir . '/Subs-Package.php');
-		package_put_contents($boarddir . '/Packages/installed.list', time());
+		require_once(SUBSDIR . '/Package.subs.php');
+		package_put_contents(BOARDDIR . '/Packages/installed.list', time());
 	}
+
 	if (isset($_POST['remove_hooks']))
+	{
 		remove_hooks();
+	}
 
 	$context['sub_template'] = 'admin';
 	$context['page_title'] = $txt['log_packages_title_' . (!empty($context['install']) ? 'installed' : 'removed')];
-	// Making a list is not hard with this beauty.
-	require_once($sourcedir . '/Subs-List.php');
 
 	// Use the standard templates for showing this.
 	$listOptions = array(
@@ -185,34 +224,36 @@ function fixp_main ()
 					'value' => $txt['mod_' . (!empty($context['install']) ? 'installed' : 'removed')],
 				),
 				'data' => array(
-					'function' => create_function('&$data', '
-						return timeformat($data[\'time_' . (!empty($context['install']) ? 'installed' : 'removed') . '\']);
-					'),
-				),
+					'function' => function (&$data) {
+						global $context;
+
+						return standardTime($data['time_' . (!empty($context['install']) ? 'installed' : 'removed')]);
+					}
+				)
 			),
 			'check' => array(
 				'header' => array(
 					'value' => '<input type="checkbox" onclick="invertAll(this, this.form);" class="input_check" />',
 				),
 				'data' => array(
-					'function' => create_function('$data', '
-						return \'<input type="checkbox" name="remove[]" value="\' . $data[\'id_install\'] . \'"  class="input_check" />\';
-					'),
+					'function' => function ($data) {
+						return '<input type="checkbox" name="remove[]" value="' . $data['id_install'] . '"  class="input_check" />';
+					},
 					'class' => 'centertext',
 				),
 			),
 		),
 		'form' => array(
-			'href' => $boardurl . '/fix_packages.php?' . $context['session_var'] . '=' . $context['session_id'] . (!empty($context['install']) ? '' : ';uninstall'),
+			'href' => $boardurl . '/fixp.php?' . $context['session_var'] . '=' . $context['session_id'] . (!empty($context['install']) ? '' : ';uninstall'),
 		),
 		'additional_rows' => array(
 			array(
 				'position' => 'below_table_data',
+				'class' => 'submitbutton',
 				'value' => '
-				<a href="' . $boardurl . '/fix_packages.php' . (!empty($context['install']) ? '?uninstall' : '') . '">[ ' . (!empty($context['install']) ? $txt['uninstall'] : $txt['install']) . ' ]</a>
-				<input type="submit" name="remove_packages" value="' . $txt['pack_button_' . (!empty($context['install']) ? 'remove' : 'install')] . '" class="button_submit" />
-				<input type="submit" name="remove_hooks" value="' . $txt['remove_hooks'] . '" class="button_submit" />',
-				'class' => 'righttext',
+				<a class="linkbutton" href="' . $boardurl . '/fixp.php' . (!empty($context['install']) ? '?uninstall' : '') . '">' . (!empty($context['install']) ? $txt['uninstall'] : $txt['install']) . '</a>
+				<input type="submit" name="remove_packages" value="' . $txt['pack_button_' . (!empty($context['install']) ? 'remove' : 'install')] . '" />
+				<input type="submit" name="remove_hooks" value="' . $txt['remove_hooks'] . '" />',
 			),
 		),
 	);
@@ -224,47 +265,66 @@ function fixp_main ()
 	createList($listOptions);
 }
 
-function list_getPacks ()
+/**
+ * Fetch the packages that are logged in the system
+ *
+ * @return array
+ */
+function list_getPacks()
 {
-	global $smcFunc, $context;
+	global $context;
+
+	$db = database();
 
 	$request = $db->query('', '
-		SELECT id_install, name, version, time_installed, time_removed
+		SELECT 
+			id_install, name, version, time_installed, time_removed
 		FROM {db_prefix}log_packages
 		WHERE install_state = {int:inst_state}
 		ORDER BY id_install',
 		array(
 			'inst_state' => $context['install'],
-	));
+		));
 	$installed = array();
 	while ($row = $db->fetch_assoc($request))
+	{
 		$installed[] = $row;
-
+	}
 	$db->free_result($request);
 
 	return $installed;
 }
 
-function list_getNumPacks ()
+/**
+ * How many packages are installed
+ * @return mixed
+ */
+function list_getNumPacks()
 {
-	global $smcFunc, $context;
+	global $context;
+
+	$db = database();
 
 	$request = $db->query('', '
-		SELECT COUNT(*)
+		SELECT 
+			COUNT(*)
 		FROM {db_prefix}log_packages
 		WHERE install_state = {int:inst_state}',
 		array(
 			'inst_state' => $context['install'],
-	));
+		));
 	list ($numPacks) = $db->fetch_row($request);
 	$db->free_result($request);
 
 	return $numPacks;
 }
 
+/**
+ * Remove all hooks
+ */
 function remove_hooks()
 {
-	global $smcFunc;
+	$db = database();
 
 	$db->query('', '
 		DELETE FROM {db_prefix}settings
@@ -274,6 +334,6 @@ function remove_hooks()
 		)
 	);
 
-	// Now fixing the cache...
-	cache_put_data('modsettings', null, 0);
+	// Kill the cache - it needs redoing now.
+	Cache::instance()->remove('modSettings');
 }
